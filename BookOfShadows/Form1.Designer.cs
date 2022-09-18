34083a2aed5487dff705461152b7f76cf7a84260
Kyle Martinez <elyk17@yahoo.com>
2022-09-18 14:56:47 -0600

    Initial commit with design for logging in.

---
+++ BookOfShadows/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace BookOfShadows
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openButton = new System.Windows.Forms.Button();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.openButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.openButton.Image = ((System.Drawing.Image)(resources.GetObject("openButton.Image")));
+            this.openButton.Location = new System.Drawing.Point(455, 475);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(145, 53);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.passwordLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.passwordLabel.Image = ((System.Drawing.Image)(resources.GetObject("passwordLabel.Image")));
+            this.passwordLabel.Location = new System.Drawing.Point(202, 419);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(186, 37);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "Please Enter Password";
+            this.passwordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.passwordLabel.UseWaitCursor = true;
+            this.passwordLabel.Visible = false;
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.passwordBox.Location = new System.Drawing.Point(212, 475);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.PasswordChar = '*';
+            this.passwordBox.Size = new System.Drawing.Size(176, 23);
+            this.passwordBox.TabIndex = 2;
+            this.passwordBox.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(647, 904);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.openButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button openButton;
+        private Label passwordLabel;
+        private TextBox passwordBox;
+    }
+}
---
+++ BookOfShadows/Form1.cs
@@ -0,0 +1,17 @@
+namespace BookOfShadows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            passwordLabel.Visible = true;
+            passwordBox.Visible = true;
+
+        }
+    }
+}
